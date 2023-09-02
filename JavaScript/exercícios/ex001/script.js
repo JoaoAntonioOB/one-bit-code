e47acc5ca1c17e81d7140d2dfec580d07aa4b4e1
João Antônio <joao.antonio2452@outlook.com>
2023-09-02 13:41:54 -0300

    Montagem do HTML

---
+++ JavaScript/exercícios/ex001/script.js
@@ -0,0 +1,7 @@
+let firstName = document.getElementById("fistName");
+let lastName = document.getElementById("lastName");
+let study = document.getElementById("study");
+let birth = document.getElementById("birth")
+let output = document.getElementById("output")
+
+output.innerHTML("Olá meus amigos")
