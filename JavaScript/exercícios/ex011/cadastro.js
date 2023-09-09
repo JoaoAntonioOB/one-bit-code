af9551568986a2e3e5352e74aa9353d07b81d25a
João Antônio <joao.antonio2452@outlook.com>
2023-09-09 13:12:47 -0300

    Começando projeto Devs

---
+++ JavaScript/exercícios/ex011/cadastro.js
@@ -0,0 +1,28 @@
+function createLabel(text){
+    const label = document.createElement('label')
+    label.innerText = text
+    return label
+}
+
+function createInput(id, value, name, type = 'text', placeholder = '') {
+    const input = document.createInput('input')
+    label.id = id
+    label.value = value
+    label.name = name
+    label.type = type
+    label.placeholder = placeholder
+    return input
+}
+
+const addTechBtn = document.getElementById('addTechBtn')
+const form = document.getElementById('devForm')
+const developers = []
+const newRow = 0
+
+addTechBtn.addEventListener('click', function(event){
+    const newTech = document.getElementById('newTech')
+
+    const newRow = document.createElement('li')
+    
+
+})
